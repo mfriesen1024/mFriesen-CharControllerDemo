115ccc0ed6fca21d453add7dde02dcfb55d2dc13
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-01-15 12:03:25 -0400

    Test movement values

--- Assets/CharacterController2D.cs
+++ Assets/CharacterController2D.cs
@@ -1,12 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] float primaryMoveSpeed;
     [SerializeField] Rigidbody2D rb;
+    Vector2 move;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        move = value.Get<Vector2>();
+        Debug.Log(move.normalized);
     }
 }
